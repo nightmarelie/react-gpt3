15e0b984b1ba74836713d6bf2be3648fe8cad151
Oleksii Kryvtsov <nightmarelie@gmail.com>
2022-06-22 21:16:52 -0600

    feat: add article

--- src/components/article/Article.tsx
+++ src/components/article/Article.tsx
@@ -8,5 +8,18 @@ type Props = {
 };
 
 export const Article: FC<Props> = ({ imgUrl, date, text }) => {
-  return <div>Article</div>;
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog_image" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{text}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
